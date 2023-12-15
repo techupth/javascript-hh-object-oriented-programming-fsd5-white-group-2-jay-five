ab0b2661a5cdda4a076206398acd5cedb7485d21
FiveTW87 <thanwalokwattanakul@gmail.com>
2023-12-15 13:27:16 +0700

    feat:complete Ex

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,38 @@
-class EmailNotification { }
+class EmailNotification {
+  constructor(notificatiobId, crearedTime, content, receiver) {
+    this.notificatiobId = notificatiobId;
+    this.crearedTime = crearedTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.receiver}`);
+  }
+}
 
-class SMSNotification { }
+class SMSNotification {
+  constructor(notificatiobId, crearedTime, content, phoneNumber) {
+    this.notificatiobId = notificatiobId;
+    this.crearedTime = crearedTime;
+    this.content = content;
+    this.phoneNumber = phoneNumber;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.phoneNumber}`);
+  }
+}
+
+const testNotification = new EmailNotification(
+  "1234",
+  "12.46",
+  "test-noti",
+  "thanwalokwattanakul@gmail.com"
+);
+testNotification.send();
+const testSMSNotification = new SMSNotification(
+  "1234",
+  "12.41",
+  "test-noti",
+  "094XXXXXXX"
+);
+testSMSNotification.send();
